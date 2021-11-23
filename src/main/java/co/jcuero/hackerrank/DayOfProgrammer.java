f61963ce061c79a454455110bdbc3c4efaa13dd8
Fary Hurtado <hurtadofary@gmail.com>
2021-11-22 23:25:09 -0500

    Day of the Programmer

--- src/main/java/co/jcuero/hackerrank/DayOfProgrammer.java
+++ src/main/java/co/jcuero/hackerrank/DayOfProgrammer.java
@@ -9,8 +9,42 @@ public class DayOfProgrammer {
     private DayOfProgrammer() {
     }
 
+    private static boolean isDivisible(int year, int n) {
+        return year % n == 0;
+    }
+
+    private static boolean isLeapYearInJuliaCalendar(int year) {
+        return isDivisible(year, 4);
+    }
+
+    private static boolean isLeapYearInGregorianCalendar(int year) {
+        return isDivisible(year, 400) || (isDivisible(year, 4) && !isDivisible(year, 100));
+    }
+
+    private static int getDaysInFebruary(int year, boolean isLeapYear) {
+        int daysInFebruary = isLeapYear ? 29 : 28;
+
+        if (year == 1918) {
+            daysInFebruary = daysInFebruary - 13;
+        }
+
+        return daysInFebruary;
+    }
+
     public static String dayOfProgrammer(int year) {
+        boolean isJulia = year >= 1700 && year <= 1917;
+        boolean isGregorian = year >= 1918 && year <= 2700;
+        int daysInFebruary;
 
+        if (isJulia) {
+            daysInFebruary = getDaysInFebruary(year, isLeapYearInJuliaCalendar(year));
+        } else if (isGregorian) {
+            daysInFebruary = getDaysInFebruary(year, isLeapYearInGregorianCalendar(year));
+        } else {
+            return null;
+        }
 
+        int days = 31 + daysInFebruary + 31 + 30 + 31 + 30 + 31 + 31;
+        return (256 - days) + ".09." + year;
     }
 }
--- src/test/java/co/jcuero/hackerrank/DayOfProgrammerTest.java
+++ src/test/java/co/jcuero/hackerrank/DayOfProgrammerTest.java
@@ -12,12 +12,66 @@ public class DayOfProgrammerTest {
 
 
     @Test
-    public void test() {
-        int year = 2;
+    public void test_2017() {
+        int year = 2017;
 
         String result = DayOfProgrammer.dayOfProgrammer(year);
 
-        Assert.assertEquals("Cat B", result);
+        Assert.assertEquals("13.09.2017", result);
+    }
+
+    @Test
+    public void test_2016() {
+        int year = 2016;
+
+        String result = DayOfProgrammer.dayOfProgrammer(year);
+
+        Assert.assertEquals("12.09.2016", result);
+    }
+
+    @Test
+    public void test_1984() {
+        int year = 1984;
+
+        String result = DayOfProgrammer.dayOfProgrammer(year);
+
+        Assert.assertEquals("12.09.1984", result);
+    }
+
+    @Test
+    public void test_1800() {
+        int year = 1800;
+
+        String result = DayOfProgrammer.dayOfProgrammer(year);
+
+        Assert.assertEquals("12.09.1800", result);
+    }
+
+    @Test
+    public void test_1918() {
+        int year = 1918;
+
+        String result = DayOfProgrammer.dayOfProgrammer(year);
+
+        Assert.assertEquals("26.09.1918", result);
+    }
+
+    @Test
+    public void test_1700() {
+        int year = 1700;
+
+        String result = DayOfProgrammer.dayOfProgrammer(year);
+
+        Assert.assertEquals("12.09.1700", result);
+    }
+
+    @Test
+    public void test_2700() {
+        int year = 2700;
+
+        String result = DayOfProgrammer.dayOfProgrammer(year);
+
+        Assert.assertEquals("13.09.2700", result);
     }
 
 }
